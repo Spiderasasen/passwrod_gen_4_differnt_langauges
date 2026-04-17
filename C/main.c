d1d6e053b8d2b3363ddbf1d5da856746588fb961
spiderasasen <spiderasasen@gmail.com>
2026-04-17 08:55:03 -0400

    got the options selections done

--- C/main.c
+++ C/main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    //asking the user, how long the password should be
+    //private vars
     int length_word = 0;
+    char password_option;
+
+    //asking the user, how long the password should be
     printf("How long would you like the word to be?\n");
     if ((scanf("%d", &length_word) != 1) || (length_word <= 0)) {
+        printf("Invalid input\n");
+    }
+
+    //asking the user what type of password they want
+    printf("What option would you like?\n"
+           "a. Password of letters only\n"
+           "b. Password or numbers only\n"
+           "c. Password of spical characters\n"
+           "d. Password with all\n");
+    scanf(" %c", &password_option);
+    if (!(password_option == 'a' || password_option == 'b' || password_option == 'c' || password_option == 'd')) {
         printf("Invalid input\n");
     }
 }
