7d3efbf6c712ca60c74ec2d0e977ef40b46c40e2
spiderasasen <spiderasasen@gmail.com>
2026-04-17 08:43:46 -0400

    made a checker for the user input in C

--- C/main.c
+++ C/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!\n");
+    //asking the user, how long the password should be
+    int length_word = 0;
+    printf("How long would you like the word to be?\n");
+    if ((scanf("%d", &length_word) != 1) || (length_word <= 0)) {
+        printf("Invalid input\n");
+    }
 }
