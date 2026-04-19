0df946f1529620bcbd343e7cf325626f0f2d01f0
spiderasasen <spiderasasen@gmail.com>
2026-04-19 19:23:39 -0400

    made the system to genorate the correct area

--- java/src/Main.java
+++ java/src/Main.java
@@ -45,5 +45,6 @@ public class Main {
         //calling the setters
         pas.setting_length(password_length);
         pas.set_option(password_option);
+        pas.genoration();
     }
 }
--- java/src/password_genoration.java
+++ java/src/password_genoration.java
@@ -1,6 +1,7 @@
 public class password_genoration {
     private int passwordLength = 0;
     private char passwordOption;
+    private String password;
 
     //setters
     public void setting_length(int passwordLength){
@@ -8,5 +9,22 @@ public class password_genoration {
     }
     public void set_option(char passwordOption){
         this.passwordOption = passwordOption;
+    }
+
+    public void genoration(){
+        switch(passwordOption){
+            case 'a':
+                System.out.println("letters only");
+                break;
+            case 'b':
+                System.out.println("numbers only");
+                break;
+            case 'c':
+                System.out.println("special only");
+                break;
+            case 'd':
+                System.out.println("all sections");
+                break;
+        }
     }
 }
