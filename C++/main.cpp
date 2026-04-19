2589084ba1c5ee9c82397ce4d82f1cdfed8fe51e
spiderasasen <spiderasasen@gmail.com>
2026-04-19 12:19:04 -0400

    added the manual checker for numbers in c++

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+
+    //private vars
+    int password_length = 0;
+    char password_option;
+
+    //loop to check if the input is really a number
+    cout<< "enter a password length:" << endl;
+    while (!(cin >> password_length)) {
+        cout<< "enter a password length:" << endl;
+        cin.clear();
+        cin.ignore(numeric_limits< streamsize>::max(), '\n');
+    }
+
+    cout << "number entered: " << password_length << endl;
+
     return 0;
 }
