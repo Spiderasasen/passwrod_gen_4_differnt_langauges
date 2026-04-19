b9c75579d666e8904cb1889af8cbf088ee3c1ce3
spiderasasen <spiderasasen@gmail.com>
2026-04-19 19:33:39 -0400

    opening files is done

--- java/src/password_genoration.java
+++ java/src/password_genoration.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class password_genoration {
     private int passwordLength = 0;
     private char passwordOption;
@@ -11,10 +16,29 @@ public class password_genoration {
         this.passwordOption = passwordOption;
     }
 
+    private ArrayList<Character> opening_files(String file) {
+        ArrayList<Character> list = new ArrayList<>();
+
+        try (Scanner fileReader = new Scanner(new File(file))) {
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine().trim();
+                if (!line.isEmpty()) {
+                    list.add(line.charAt(0));
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found: " + file);
+        }
+
+        return list;
+    }
+
     public void genoration(){
         switch(passwordOption){
             case 'a':
                 System.out.println("letters only");
+                ArrayList<Character> list = opening_files("data/alphabet.txt");
+                System.out.println(list);
                 break;
             case 'b':
                 System.out.println("numbers only");
