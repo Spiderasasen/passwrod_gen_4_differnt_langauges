7f53f53ca4d557985f2a962e02c9da445349f11e
spiderasasen <spiderasasen@gmail.com>
2026-04-19 18:03:38 -0400

    got the open files function to work

--- C++/password_generator.cpp
+++ C++/password_generator.cpp
@@ -4,6 +4,9 @@
 
 #include "password_generator.h"
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -18,20 +21,48 @@ void genorator::setting_option(char option) {
     password_option = option;
 }
 
+vector<char> genorator::open_files(string file) {
+    ifstream fio(file);
+
+    vector<char> items;
+
+    if (fio.fail()) {
+        cout << "error opening file" << endl;
+        return items;
+    }
+    else {
+        char c;
+        while (fio.get(c)) {
+            items.push_back(c);
+        }
+        fio.close();
+    }
+    return items;
+}
+
 //main section for new password
 void genorator::gen_password() {
     switch (password_option) {
-        case 'a':
+        case 'a': {
             cout << "letters only" << endl;
+            vector<char> items = open_files("data/alphabet.txt");
+            for (int i = 0; i < items.size(); i++) {
+                cout << items[i];
+            }
+            cout << endl;
             break;
-        case 'b':
+        }
+        case 'b': {
             cout << "numbers only" << endl;
             break;
-        case 'c':
+        }
+        case 'c': {
             cout << "special chars only" << endl;
             break;
-        case 'd':
+        }
+        case 'd': {
             cout << "all sections" << endl;
             break;
+        }
     }
 }
--- C++/password_generator.h
+++ C++/password_generator.h
@@ -4,11 +4,17 @@
 
 #ifndef PASSWROD_GEN_4_DIFFERNT_LANGAUGES_PASSWORD_GENERATOR_H
 #define PASSWROD_GEN_4_DIFFERNT_LANGAUGES_PASSWORD_GENERATOR_H
+#include <string>
+#include <vector>
+#include <istream>
+
+using namespace std;
 
 class genorator {
     private:
     int password_length = 0;
     char password_option;
+    vector<char> open_files(string file);
     public:
     void setting_length(int length);
     void setting_option(char option);
