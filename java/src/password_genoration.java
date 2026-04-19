a4f235dfe2d38ce13e1f0e790f5c40a6c3779790
spiderasasen <spiderasasen@gmail.com>
2026-04-19 19:49:07 -0400

    done with java

--- java/src/password_genoration.java
+++ java/src/password_genoration.java
@@ -75,6 +75,29 @@ public class password_genoration {
                 break;
             case 'd':
                 System.out.println("all sections");
+
+                Random rand = new Random();
+
+                ArrayList<Character> leters = opening_files("data/alphabet.txt");
+                ArrayList<Character> numbers = opening_files("data/numbers.txt");
+                ArrayList<Character> specials = opening_files("data/special.txt");
+
+                for(int i = 0; i < passwordLength; i++){
+                    int number = rand.nextInt(1, 4);
+                    switch(number){
+                        case 1:
+                            password = new_password(password, 1, leters);
+                            break;
+                        case 2:
+                            password = new_password(password, 1, numbers);
+                            break;
+                        default:
+                            password = new_password(password, 1, specials);
+                    }
+                }
+
+                System.out.println("Your password: " + password);
+
                 break;
         }
     }
