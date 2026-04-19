7cad7606d17c11817cc31cf6aa30b796b0769c79
spiderasasen <spiderasasen@gmail.com>
2026-04-19 19:16:53 -0400

    got the main section of the creation section

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,42 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.lang.Character;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //calling the scanner
+        Scanner scan = new Scanner(System.in);
+
+        //private vars
+        int password_length = 0;
+        char password_option;
+        String[] options = {"a. Password of letters only", "b. Password or numbers only", "c. Password of spical characters", "d. Password with all"};
+
+        //checking if the number is a number
+        while (true){
+            System.out.println("Please enter the length of the password: ");
+            try{
+                password_length = scan.nextInt();
+                break;
+            }
+            catch (InputMismatchException e){
+                scan.nextLine();
+            }
+        }
+
+        //for the option tag
+        while (true){
+            System.out.println("Please select an option: ");
+            for (String option : options){
+                System.out.println(option);
+            }
+            char choice = scan.next().charAt(0);
+            choice = Character.toLowerCase(choice);
+            if (!(choice == 'a' || choice == 'b' || choice == 'c' || choice == 'd')){
+                continue;
+            }
+            password_option = choice;
+            break;
+        }
     }
 }
