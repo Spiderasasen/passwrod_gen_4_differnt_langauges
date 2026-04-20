5d8b433b92522ab38f540fa5eb96357e7e598a95
spiderasasen <spiderasasen@gmail.com>
2026-04-20 16:48:42 -0400

    made a reading file section

--- Go/password_gen.go
+++ Go/password_gen.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // vars
 var password_length_private int
@@ -15,10 +19,41 @@ func settingOptions(options string) {
 	password_options_private = options
 }
 
+// opening a file
+func opening_file(filename string) ([]string, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// convert file contents into a slice of strings
+	lines := strings.Split(string(data), "\n")
+
+	// trim whitespace + remove empty lines
+	var result []string
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line != "" {
+			result = append(result, line)
+		}
+	}
+
+	return result, nil
+}
+
+// main function
 func mainGen() {
 	switch password_options_private {
 	case "a":
 		fmt.Println("letters only")
+
+		items, err := opening_file("../data/alphabet.txt")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(items)
+
 		break
 	case "b":
 		fmt.Println("numbers only")
