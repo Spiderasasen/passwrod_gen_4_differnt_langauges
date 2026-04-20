8bf7951c8baf0d8a24bb554afdb9b500a2d16ac0
spiderasasen <spiderasasen@gmail.com>
2026-04-20 15:09:09 -0400

    got the main scanner for getting the numbers done

--- Go/main.go
+++ Go/main.go
@@ -5,5 +5,23 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	//private vars
+	var passwordLength int
+	//var passwordOption string
+
+	//for the password length
+	for {
+
+		fmt.Println("Please enter the password length: ")
+
+		//checking if the scan was a number
+		_, err := fmt.Scan(&passwordLength)
+		if err != nil {
+			fmt.Println("Invalid password, please press y to continue ")
+			var trash string
+			fmt.Scan(&trash)
+			continue
+		}
+		break
+	}
 }
