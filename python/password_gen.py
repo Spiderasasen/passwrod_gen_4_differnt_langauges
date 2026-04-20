26383b9478c64df57cc492c527c954c250e3e9c3
spiderasasen <spiderasasen@gmail.com>
2026-04-20 08:59:34 -0400

    made the main making a new password system

--- python/password_gen.py
+++ python/password_gen.py
@@ -1,9 +1,10 @@
 from typing import List
+import random
 
 #private vars
 password_length: int
 password_option: chr
-password: str
+password: str = ""
 
 #setters
 def setting_length(length: int):
@@ -23,20 +24,45 @@ def open_file(file: str):
 
     return listing
 
+#makes the new password
+def new_password(new_password_string: str, length: int, items) -> str:
+    for i in range(length):
+        rand: int = random.randint(0, len(items) - 1)
+        new_password_string += items[rand]
+    return new_password_string
+
+#making the system to make a new password
+def making_password(new_password_string: str, length: int, file: str) -> str:
+    items = open_file(file)
+    new_password_string = new_password(new_password_string, length, items)
+    return new_password_string
 
 #genorator
 def generate_password():
+    #calling the private vars
     global password_option
+    global password_length
+    global password
+
     match password_option:
         case 'a':
             print("letters only")
 
-            items = open_file("data/alphabet.txt")
-            print(items)
+            password = making_password(password, password_length, "data/alphabet.txt")
+
+            print("New password:", password)
 
         case 'b':
             print("numbers only")
+
+            password = making_password(password, password_length, "data/numbers.txt")
+            print("New password:", password)
+
         case 'c':
             print("special characters only")
+
+            password = making_password(password, password_length, "data/special.txt")
+            print("New password:", password)
+
         case 'd':
             print("all elements")
