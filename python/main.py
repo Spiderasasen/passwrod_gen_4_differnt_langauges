7f8c0ad0a767ed4a76518d437114a6d68dfa45e0
spiderasasen <spiderasasen@gmail.com>
2026-04-20 08:30:45 -0400

    main system is done

--- python/main.py
+++ python/main.py
@@ -2,8 +2,32 @@ def main():
     #private vars
     password_length: int = 0
     password_option = ''
+    options = {"a. Password of letters only", "b. Password or numbers only", "c. Password of spical characters", "d. Password with all"}
 
     #checking if the length is a number
+    while password_length == 0 or password_length < 0:
+        try:
+            password_length = int(input("Enter password length: "))
+        except ValueError:
+            password_length = 0
+
+    #seeing what option the user selected
+    while password_option == '':
+        print("Please choose an option: \n")
+        for option in options:
+            print(option)
+
+        password_option = input("Enter option: ")
+        if is_valid_option(password_option):
+            password_option = ''
+            continue
+
+        break
+
+def is_valid_option(option: chr) -> bool:
+    if option == 'a' or option == 'b' or option == 'c' or option == 'd':
+        return False
+    return True
 
 
 if __name__ == '__main__':
