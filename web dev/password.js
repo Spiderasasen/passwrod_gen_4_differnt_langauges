b1bac2b387d4e4c86ba0e28eaf1b9ae9f4b9791a
spiderasasen <spiderasasen@gmail.com>
2026-04-21 13:43:50 -0400

    finished the code

--- web dev/password.js
+++ web dev/password.js
@@ -2,7 +2,7 @@ const submit_button = document.getElementById("submit");
 const password_length = document.getElementById("password_length");
 const password_option = document.getElementById("password_options");
 let password = "";
-const password_area = document.getElementById("password_area");
+const password_area = document.getElementById("password_space");
 
 submit_button.addEventListener("click", async () => {
     //confirmation that submit has been clicked
@@ -26,20 +26,58 @@ submit_button.addEventListener("click", async () => {
             password = making_password(password, password_length.value, letters);
 
             console.log(password);
+            password_area.textContent = "Your Password: " + password;
 
             break;
         case "b":
             console.log("Numbers Only");
+
+            //making the password
+            password = making_password(password, password_length.value, numbers);
+            console.log(password);
+            password_area.textContent = "Your Password: " + password;
+
             break;
         case "c":
             console.log("Specials Only");
+
+            //making the password
+            password = making_password(password, password_length.value, specials);
+            console.log(password);
+            password_area.textContent = "Your Password: " + password;
+
             break;
         case "d":
             console.log("All options");
+
+            for (let i = 0; i < password_length.value; i++) {
+                let rand = Math.floor(Math.random() * 3);
+
+                switch(rand) {
+                    case 0: //for letters
+                        password = making_password(password, 1, letters);
+                        break;
+                    case 1:
+                        password = making_password(password, 1, numbers);
+                        break;
+                    case 2:
+                        password = making_password(password, 1, specials);
+                        break;
+                }
+            }
+
+            console.log(password);
+            password_area.textContent = "Your Password: " + password;
+
             break;
         default:
             console.log("No selection");
+            password_area.textContent = "Please choose an option";
     }
+
+    //clearing out the inputs
+    password_option.value = "";
+    password = "";
 });
 
 //making the password
