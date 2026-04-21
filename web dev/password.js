46860cbc580cfc8f373b050d61a323f67326fe8f
spiderasasen <spiderasasen@gmail.com>
2026-04-21 09:22:45 -0400

    got the file reader working also had to make a copy data file so it can read the file outside inside JS

--- web dev/password.js
+++ web dev/password.js
@@ -2,7 +2,7 @@ const submit_button = document.getElementById("submit");
 const password_length = document.getElementById("password_length");
 const password_option = document.getElementById("password_options");
 
-submit_button.addEventListener("click", () => {
+submit_button.addEventListener("click", async () => {
     //confirmation that submit has been clicked
     console.log("Submit has been clicked!");
 
@@ -10,4 +10,31 @@ submit_button.addEventListener("click", () => {
     console.log("User password length: ", password_length.value);
     console.log("User password option: ", password_option.value);
 
+    //checking what option the user selcted
+    switch (password_option.value) {
+        case "a":
+            console.log("Letters Only");
+
+            const items = await opening_file("data/alphabet.txt");
+
+
+            break;
+        case "b":
+            console.log("Numbers Only");
+            break;
+        case "c":
+            console.log("Specials Only");
+            break;
+        case "d":
+            console.log("All options");
+            break;
+        default:
+            console.log("No selection");
+    }
 });
+
+async function opening_file(filename){
+    const response = await fetch(filename);
+    const text = await response.text();
+    return text.split("\n").map(line => line.trim()).filter(Boolean);
+}
