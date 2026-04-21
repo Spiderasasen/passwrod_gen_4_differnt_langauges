b7164dda412ea80ec83fa8b2dd84e28e55b7a2c7
spiderasasen <spiderasasen@gmail.com>
2026-04-21 09:07:37 -0400

    got the values of the objects

--- web dev/password.js
+++ web dev/password.js
@@ -1,5 +1,13 @@
 const submit_button = document.getElementById("submit");
+const password_length = document.getElementById("password_length");
+const password_option = document.getElementById("password_options");
 
 submit_button.addEventListener("click", () => {
+    //confirmation that submit has been clicked
     console.log("Submit has been clicked!");
+
+    //checking on the selctions
+    console.log("User password length: ", password_length.value);
+    console.log("User password option: ", password_option.value);
+
 });
